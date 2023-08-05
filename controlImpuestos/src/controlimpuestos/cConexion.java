af0005c2c448d7f666d9d82a1d6e8d0e84dc9aca
Daniel <dbarquero70479@ufide.ac.cr>
2023-08-05 17:53:40 -0600

    fix cConexion (pagos)

--- controlImpuestos/src/controlimpuestos/cConexion.java
+++ controlImpuestos/src/controlimpuestos/cConexion.java
@@ -8,8 +8,7 @@ public class cConexion {
     //Variables para la conexion con el servidor de base de datos (MySQL)
     public static final String DB_URL = "jdbc:mysql://localhost:3306/"; //URL de la base de datos
     public static final String DB_USER = "root"; //Usuario de la base de datos
-//    public static final String DB_PASS = "root1234"; //Contraseña de la base de datos
-    public static final String DB_PASS = "fidelitas1232ytu-";
+    public static final String DB_PASS = "root1234";
 
     public static Connection getConnection() {
         Connection conn = null;
