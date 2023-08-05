652ff2619cef7e6fcf71de5d5544249f4f0ae3dc
Daniel <dbarquero70479@ufide.ac.cr>
2023-08-05 17:08:25 -0600

    fix Pagos

--- controlImpuestos/src/controlimpuestos/cPagos.java
+++ controlImpuestos/src/controlimpuestos/cPagos.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Date;
 import javax.swing.JOptionPane;
+import java.sql.SQLException;
 
 /**
  *
@@ -13,116 +14,62 @@ import javax.swing.JOptionPane;
  */
 public class cPagos {
     
-    //Atributos faltantes
-    //Pendiente arreglar los tipos de datos de la clase y modificarla para que haga match con los campos de la base de datos
     private int idPago;
     private double monto;
+    private String fecha;
+    private double impuesto;
+    private String nombre;
 
-    //Atributos por default
-    cClientes idCliente;
-    cImpuestos tasaImpuesto;
-    cImpuestos fecha;
-    private double pagos;
-
-    public cPagos(cClientes idCliente, cImpuestos tasaImpuesto, cImpuestos fecha, double pagos) {
-        this.idCliente = idCliente;
-        this.tasaImpuesto = tasaImpuesto;
+    public cPagos(int idPago, double monto, String fecha, double impuesto, String nombre) {
+        this.idPago = idPago;
+        this.monto = monto;
         this.fecha = fecha;
-        this.pagos = pagos;
-    }
-    
-    public cClientes getIdCliente() {
-        return idCliente;
+        this.impuesto = impuesto;
+        this.nombre = nombre;
     }
 
-    public void setIdCliente(cClientes idCliente) {
-        this.idCliente = idCliente;
+    public int getId() {
+        return idPago;
     }
 
-    public cImpuestos getTasaImpuesto() {
-        return tasaImpuesto;
+    public void setId(int id) {
+        this.idPago = id;
     }
 
-    public void setTasaImpuesto(cImpuestos tasaImpuesto) {
-        this.tasaImpuesto = tasaImpuesto;
+    public double getMonto() {
+        return monto;
     }
 
-    public cImpuestos getFecha() {
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getFecha() {
         return fecha;
     }
 
-    public void setFecha(cImpuestos fecha) {
+    public void setFecha(String fecha) {
         this.fecha = fecha;
     }
 
-    public double getPagos() {
-        return pagos;
+    public double getImpuesto() {
+        return impuesto;
     }
 
-    public void setPagos(double Pagos) {
-        this.pagos = Pagos;
+    public void setImpuesto(double impuesto) {
+        this.impuesto = impuesto;
+    }
+
+    public String getCliente() {
+        return nombre;
+    }
+
+    public void setCliente(String cliente) {
+        this.nombre = cliente;
     }
 
     @Override
     public String toString() {
-        return "cPagos{" + "idCliente=" + idCliente + ", tasa_impuesto=" + tasaImpuesto + ", fecha=" + fecha + ", Pagos=" + pagos + '}';
+        return "cPagos{" + "id=" + idPago + ", monto=" + monto + ", fecha=" + fecha + ", impuesto=" + impuesto + ", cliente=" + nombre + '}';
     }
-
-   
-    //==========================================================================
-    
-    public void crearPago(){
-        
-        //conexion bd
-        
-        //busqueda de datos
-        
-        //algoritmo de creacion de pagos
-        
-        //guardar en bd
-        
-        //cerrar conexion
-        
-        
-    }
-    
-    public void buscarPago(String nombreCliente){
-      
-        try{
-        
-            Connection conn = cConexion.getConnection();        
-            String consulta = "SELECT idCliente, Impuesto, fecha, Pago FROM pagos WHERE idCliente IN (SELECT idCLiente FROM clientes WHERE nombreCliente = ?)";            
-            PreparedStatement ps = conn.prepareStatement(consulta);
-            
-            ps.setString(1, nombreCliente);
-            ResultSet res = ps.executeQuery();
-            
-            while(res.next()){
-                int idCliente = res.getInt("idCliente");
-                double impuesto = res.getDouble("Impuesto");
-                Date fecha = res.getDate("fecha");
-                double pago = res.getDouble("Pago");
-                
-                
-            //imprimir en pantalla informacion (temporal)
-            System.out.println("ID del Cliente: " + idCliente);
-            System.out.println("Impuesto correspondiente: " + impuesto);
-            System.out.println("Fecha de pago: " + fecha);
-            System.out.println("Pago: " + pago);
-            
-            }
-            
-            conn.close();
-            
-        }catch(Exception e){
-            JOptionPane.showMessageDialog(null, "Error" +e);
-        }
-    
-        //buscar Pago
-        
-        //mostrar en pantalla (interfaz)
-    
-        //cerrar conexion
-    }
-    
 }
